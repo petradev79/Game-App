ea121196f0386dae8063b2e41feb5f602490cb2a
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-02-18 15:58:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
