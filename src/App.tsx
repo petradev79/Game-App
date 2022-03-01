47eb6e452c87832ee201632ae928e94474c9e711
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-03-01 15:13:17 +0100

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import fetch from './helper/fetch';
 import requests from './helper/requests';
 import Navbar from './components/Navbar';
